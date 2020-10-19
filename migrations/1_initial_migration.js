0d6fc5d4ca13229a45cd7a0903922943ad2d4fa6
TepNik <tepelin.nikita@gmail.com>
2020-10-19 17:05:10 +0300

    Changed time in migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,6 +4,6 @@ const BN = require("bn.js");
 
 module.exports = function (deployer) {
     // 0
-    //
-    deployer.deploy(Praem, new BN(1609372800))
+    // 1610236800 -- Sun, 10 Jan 2021 00:00:00 GMT
+    deployer.deploy(Praem, new BN(0))
 };
